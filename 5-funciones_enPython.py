089a60b6d1f3fcffe1a7d4089e17557dd310557d
Luis Benitez <luisbenitez02@gmail.com>
2018-10-02 12:24:16 -0500

    Funciones en Python
    Funciones que nos brinda Python de fabrica para trabajar

---
+++ 5-funciones_enPython.py
@@ -0,0 +1,14 @@
+#vivi_data = (21,1.70,'cafes','A+','Viviana','Bonilla',True)
+#help(vivi_data)
+
+x=True
+
+print(type(x))
+
+num1=20
+num2=18
+
+print(str(num1)+str(num2))
+
+tupla=(5,10,15,122)
+print(dir(tupla))
